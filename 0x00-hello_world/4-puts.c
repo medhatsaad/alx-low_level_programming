f07c8338f0254e68278bb526fad69b0fa6d51fcc
medhatsaad <medhat.saad0@gmail.com>
2023-03-16 16:10:51 -0700

    x

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@
 /**
  * first c program
  */
-int main(void)
+int main (void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
-	puts(str);
-	return(0);
+
+	puts (str);
+	return (0);
 }
