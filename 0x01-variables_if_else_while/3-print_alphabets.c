0cedab92322002bdeead411a926e8cce2513e459
medhatsaad <medhat.saad0@gmail.com>
2023-03-17 11:55:34 -0700

    Task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print alph
+ * Return: 0 is ok
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		putchar(n);
+	
+	for (n = 'A'; n <= 'Z'; n++)
+		putchar(n);
+
+	putchar('\n');
+	return (0);
+}
+
+
+
+
