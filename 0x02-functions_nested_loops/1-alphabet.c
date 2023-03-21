9607a0aa81c062dfba66d8ae6356f83295b2d2c9
medhatsaad <medhat.saad0@gmail.com>
2023-03-21 16:07:38 -0700

    T1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alpha
+ * Return: 0 is ok
+ */
+void print_alphabet(void)
+{
+	int i;
+	for (i=97; i <=122; i++)
+		_putchar(i);
+	_putchar('\n');
+	return (0);
+}
