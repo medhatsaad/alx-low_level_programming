9afd9eaa588a3cab7ddb38ced6e03a599c5be9ec
medhatsaad <medhat.saad0@gmail.com>
2023-03-21 16:51:01 -0700

    T2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alpha x 10
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (i = 97; i <= 122; i++)
+			_putchar(i);
+		_putchar('\n');
+}	}
