e2fdcdd7df1f955919628260a3af0c1fc655f6b6
medhatsaad <medhat.saad0@gmail.com>
2023-03-21 14:39:03 -0700

    Task0-1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,15 @@
-#include <main.h>
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+#include <"main.h">
+
+
+int main (void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+}
