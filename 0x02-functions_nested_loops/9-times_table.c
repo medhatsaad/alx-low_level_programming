792ac89de149406f141c02408cce1777cf5a0f7f
medhatsaad <medhat.saad0@gmail.com>
2023-03-22 03:05:06 -0700

    T9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 		{
 			if (time == 0)
 				_putchar('0');
-			else if((num * time) / 10 == 0)
+			else if ((num * time) / 10 == 0)
 			{	
 				_putchar(' ');
 				_putchar('0' + (num * time) % 10);
