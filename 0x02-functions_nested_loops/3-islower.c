a32f74e83bb6a63156ad0c2872adf4fd0f997435
medhatsaad <medhat.saad0@gmail.com>
2023-03-21 17:51:02 -0700

    T3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,13 @@
 
 /**
  * _islower - check for lowercase
- * @c - is a character
+ * @c:  is a character
  * Return: 1 is ok otherwise 0
  */
 int _islower(int c)
 {
-	if ( c >= 97 && c <= 122 )
+	if (c >= 97 && c <= 122)
 		return (1);
 	else
-		{
 		return (0);
-		}	
 }
