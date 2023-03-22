e9d5d1d8e077132c4b11dc1d77e219b7336e3805
medhatsaad <medhat.saad0@gmail.com>
2023-03-22 16:26:38 -0700

    T100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -60,7 +60,7 @@ void print_times_table(int n)
 	int i;
 	int m;
 
-	if (n >= 0 && n < 15)
+	if (n > 0 && n < 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -81,5 +81,10 @@ void print_times_table(int n)
 			myprint(i * n);
 			_putchar('\n');
 		}
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
 	}
 }
