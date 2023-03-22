a4a1f88bf480ac9ed006fe888afed86a2b7c98e4
medhatsaad <medhat.saad0@gmail.com>
2023-03-22 04:56:58 -0700

    T11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,27 +4,10 @@
  * print_to_98 - print numbers n to 98
  * @n: int
  */
-void myprint(int m)
-{
-	if (m / 10 == 0)
-	{	
-		_putchar('0' + m % 10);
-		_putchar(',');
-		_putchar(' ');
-	}
-	else
-	{
-		_putchar('0' + m / 10);
-		_putchar('0' + m % 10);
-		_putchar(',');
-		_putchar(' ');
-	}
-}
-
 void print_to_98(int n)
 {
 	
-	while(1)
+	while (1)
 	{
 		if (n == 98)
 		{
@@ -38,12 +21,12 @@ void print_to_98(int n)
 		}
 		else if (n < 98)
 		{
-			printf("%d, ",n);
+			printf("%d, ", n);
 			n++;
 		}	
 		else
 		{
-			printf("%d, ",n);
+			printf("%d, ", n);
 			n--;
 		}
 	}
