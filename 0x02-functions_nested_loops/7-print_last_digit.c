85f71776f16caab2271b08342fdd259bf804c6b6
medhatsaad <medhat.saad0@gmail.com>
2023-03-22 01:23:48 -0700

    T7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,12 @@ int print_last_digit(int n)
 {
 	if (n >= 0)
 	{
-		_putchar('0' + n%10);
-		return (n%10);
+		_putchar('0' + n % 10);
+		return (n % 10);
 	}
 	else
 	{
-		_putchar('0' + -(n%10));
-		return (-(n%10));
+		_putchar('0' + -(n % 10));
+		return (-(n % 10));
 	}
 }
