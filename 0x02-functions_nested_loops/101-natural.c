bc6afb50ed25b122d39f6bec5cfa39d6a8ec2aac
medhatsaad <medhat.saad0@gmail.com>
2023-03-22 17:40:25 -0700

    T101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - sum divisable by 3 or 5 under 1024
+ * Return: 0 is ok
+ */
+int main(void)
+{
+	int sum;
+	int i;
+	
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0)
+			sum += i;
+		else if (i & 5 == 0)
+			sum += i;
+	}
+	printf("%d\n",sum);	
+	return (0);
+
+}
