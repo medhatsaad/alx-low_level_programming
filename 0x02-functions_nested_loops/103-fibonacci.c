54650065d6c1e5496d82d5596a7e564e1dff3ebb
medhatsaad <medhat.saad0@gmail.com>
2023-03-22 18:48:35 -0700

    T103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+# include <stdio.h>
+
+/**
+ * main - print first 50 fib no start with 1,3
+ * Return: 0 is ok
+ */
+int main(void)
+{
+	long f1;
+	long f2;
+	long f3;
+	long long sum;
+
+	f1 = 1;
+	f2 = 2;
+	sum = 2;
+	while (1)
+	{
+		f3 = f1 + f2;
+		if (f3 >= 4000000)
+			break;
+		else
+		{
+			if (f3 % 2 == 0)
+				sum = sum + f3;
+		}
+		f1 = f2;
+		f2 = f3;
+	}
+	printf("%llu\n", sum);
+	return (0);
+}
