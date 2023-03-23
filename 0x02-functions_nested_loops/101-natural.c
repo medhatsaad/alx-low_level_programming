d5631b22819181a6f324de608b664802d3e359c5
medhatsaad <medhat.saad0@gmail.com>
2023-03-22 17:43:56 -0700

    T101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int sum;
 	int i;
-	
+
 	sum = 0;
 	for (i = 0; i < 1024; i++)
 	{
@@ -16,7 +16,7 @@ int main(void)
 		else if (i % 5 == 0)
 			sum += i;
 	}
-	printf("%d\n",sum);	
+	printf("%d\n", sum);	
 	return (0);
 
 }
