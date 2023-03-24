ebb7b1be075800198babc58b2194839f6b268c37
medhatsaad <medhat.saad0@gmail.com>
2023-03-24 06:24:28 -0700

    T7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,13 +10,14 @@ void print_diagonal(int n)
 		int i;
 		int j;
 
-		for (i = 0; i < n; i++)
+		for (i = 1; i < n; i++)
 		{
 			_putchar('\\');
 			_putchar('\n');
-			for (j = 0; j < i + 1; j++)
+			for (j = 0; j < i; j++)
 				_putchar(' ');
 		}
+		_putchar('\\');
 	}
 	_putchar('\n');
 }
