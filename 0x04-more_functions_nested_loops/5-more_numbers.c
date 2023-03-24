ce9c2c37f691b35c73340a0591d57fb8de08a17c
medhatsaad <medhat.saad0@gmail.com>
2023-03-24 05:44:26 -0700

    T5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+# include "main.h"
+/**
+ * more_numbers - print numbers 0 to 14 10 times
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 10)
+				_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+		}
+		_putchar('\n');
+	}
+}
