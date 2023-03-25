25514ad045ee87283b5be1ebe0bc2906943cd974
medhatsaad <medhat.saad0@gmail.com>
2023-03-24 18:56:29 -0700

    T3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,10 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || ((year % 400 == 0) && (year % 100 == 0)))
+	int y;
+
+	y = year;
+	if ((y % 4 == 0 && !(y % 100 == 0)) || (y % 100 == 0 && y % 400 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
