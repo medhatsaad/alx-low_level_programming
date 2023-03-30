543d91b2468708c8993e991539dc491e8c2ef6c8
medhatsaad <medhat.saad0@gmail.com>
2023-03-30 07:21:18 -0700

    t

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+# include "main.h"
+/**
+ * string_toupper - change all lower case to upper
+ * @s: string
+ *
+ * Return: pointer to string
+ */
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+		if (a[i] >= 97 && a[i] <= 122)
+			a[i] = a[i] - 32;
+	return (a);
+}
