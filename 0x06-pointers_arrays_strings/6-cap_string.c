69d54897b6e0b771ff24d2580fd39720f9084255
medhatsaad <medhat.saad0@gmail.com>
2023-03-30 07:42:04 -0700

    m

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *a)
 	int i;
 
 	for (i = 0; a[i] != '\0'; i++)
-		if ((i == 0 || a[i - 1] == ' ') && (a[i] >= 97 && a[i] <= 122))
+		if ((i == 0 || a[i - 1] == ' ' || a[i-1] == '	' || a[i-1] == '\n') && (a[i] >= 97 && a[i] <= 122))
 			a[i] = a[i] - 32;
 	return (a);
 
