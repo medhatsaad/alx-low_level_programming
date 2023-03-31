197ffe5bbf11d92fb11d21bb6e25c75adbd59f46
medhatsaad <medhat.saad0@gmail.com>
2023-03-30 17:34:19 -0700

    m

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+# include "main.h"
+/**
+ * rot13 - code
+ *
+ * @a: string pointer
+ *
+ * Return: string
+ */
+char *rot13(char *a)
+{
+	int j, k;
+	char n[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+	char p[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+			'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+	for (j = 0; a[j] != '\0'; j++)
+	{
+		for (k = 0; p[k] != '\0'; k++)
+			if (a[j] == p[k])
+			{
+				a[j] = n[k];
+				break;
+			}
+	}
+	return (a);
+}
