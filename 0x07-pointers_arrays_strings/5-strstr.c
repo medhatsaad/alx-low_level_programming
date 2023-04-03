b5a3b6e23c4ee5991c1fab4cdd2317a5bff66742
medhatsaad <medhat.saad0@gmail.com>
2023-04-03 09:00:09 -0700

    m

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+# include "main.h"
+/**
+ * _strstr - find str in str
+ *
+ * @haystack: string to search in
+ * @needle: string to find
+ *
+ * Return: pointer to first occurence
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	char *s = haystack;
+
+	for (j = 0; haystack[j] != '\0'; j++)
+	{
+		i = 0;
+		while (s[i] != needle[0])
+		{
+			i++;
+		}
+		for (j = 1; needle[j] != '\0'; j++)
+		{
+			if (needle[j] != haystack[i + j])
+				break;
+		}
+		if (needle[j] == '\0')
+			return (haystack + i);
+
+		s++;
+	}
+	return ('\0');
+}
