7b246d8850baceba10d870a26442742c18e74ecb
medhatsaad <medhat.saad0@gmail.com>
2023-04-11 17:55:20 -0700

    m

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,14 +14,12 @@ char *_strdup(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 		size++;
-	size--;
 	a = malloc(size * sizeof(char));
 
 	if (a)
 	{
-		for (i = 0; i < size; i++)
+		for (i = 0; i <= size; i++)
 			a[i] = str[i];
-		a[size] = '\0';
 		return (a);
 	}
 	else
