261e3e8ed7749ad51da6eb6ffefc4b97fa4f4c97
medhatsaad <medhat.saad0@gmail.com>
2023-04-13 13:19:30 +0200

    m

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,15 @@
+# include "main.h"
+# include <stdlib.h>
+/**
+ * string_nooncat - concate 2 strings
+ *
+ * @s1: s1
+ * @s2: s2
+ * @n: int
+ *
+ * Return: string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	
+}
