e40e7aae7302fd1b285501d410ac869c78c6d500
medhatsaad <medhat.saad0@gmail.com>
2023-04-12 19:46:54 -0700

    m

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,5 +10,42 @@
  */
 char **strtow(char *str)
 {
+	int i;
+	int row = 0;
+	int col = 0;
+	int k = 0;
+	char **a;
+
+	if (str == NULL || str== "")
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+			row++;
+	}
+
+	a = (char **) malloc((row + 1)*sizeof(char *));
+	if (a)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			if (str[i] != ' ')
+				col++;
+			if (str[i] == ' ')
+				a[k] = (char *) malloc((col + 1) * sizeof(char));
+				if (a[k])
+				{
+					k++;
+					col = 0;
+				}
+				else
+					return (NULL);
+		}
+
+
+			
+	}
+	else
+		return (NULL);
 
 }
