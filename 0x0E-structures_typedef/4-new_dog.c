23b4ad686144bcaf77ab651a2eda8dd44aab0b81
medhatsaad <medhat.saad0@gmail.com>
2023-04-18 01:48:21 +0200

    m

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 # include "dog.h"
 # include <stdlib.h>
+# include <stdio.h>
 /**
  * new_dog - create new dog
  * @name: dog name
@@ -9,24 +10,45 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *a = name;
-	char *b = owner;
+	char *a ;
+	char *b ;
+	int sizea, sizeb;
 	dog_t *c;
 
-	c = malloc(namesize(a) + namesize(b) + 4);
+	printf("start");
+	a = name;
+	b = owner;
+	sizea = 0;
+	sizeb = 0;
+	/* (a == NULL) ? sizea = 0 : (sizea = namesize(a));
+	(b == NULL) ? sizeb = 0 : (sizeb = namesize(b)); */
+	c = (dog_t *) malloc(sizea + sizeb + 2 + 4);
 	if (c)
 	{
-		c->name = a;
+		(a == NULL) ? c->name[0] = '\0' : copyname(a, sizea, c->name);
 		c->age = age;
-		c->owner = b;
+		(b == NULL) ? c->owner[0] = '\0' : copyname(b, sizeb, c->owner);
 		return (c);
 	}
 	else
 		return (NULL);
+}	
 
+/**
+ * copyname - copy string
+ * @x: source string
+ * @size: size
+ * @y: destination
+ */
+int copyname(char *x, int size, char *y)
+{
+	int i;
 
+	for (i = 0; i < size; i++)
+		y[i] = x[i];
+	y[size] = '\0';
+	return (0);
 }
-
 /**
  * namesize - size of string
  * @s: string
@@ -34,7 +56,7 @@ dog_t *new_dog(char *name, float age, char *owner)
  */
 int namesize(char *s)
 {
-	int size = 1;
+	int size = 0;
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,5 +16,6 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 int namesize(char *s);
+int copyname(char *x, int size, char *y);
 dog_t *new_dog(char *name, float age, char *owner);
 #endif
