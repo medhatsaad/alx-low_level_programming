dfe90ad86da435c2d3ebe664dd34d03b91033834
medhatsaad <medhat.saad0@gmail.com>
2023-04-20 17:26:32 +0200

    m

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+# include <stdio.h>
+# include <stdarg.h>
+# include "variadic_functions.h"
+# include <stdlib.h>
+/**
+ * print_numbers - print list of number with separator
+ * @separator: sep between numbers
+ * @n: number of numbers
+ *
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg;
+
+	if (separator != NULL)
+	{
+		va_start(arg, n);
+		for (i = 1; i < n; i++)
+		{
+			printf("%d", va_arg(arg, int));
+			printf("%s", separator);
+		}
+		printf("%d\n", va_arg(arg, int));
+		va_end(arg);
+	}
+
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,5 @@
 # ifndef _FILE_NAME_H_
 # define _FILE_NAME_H_
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 # endif
