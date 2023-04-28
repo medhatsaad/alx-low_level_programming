f425d8203bd2df1032459619e9a25531dcdcf1ab
medhatsaad <medhat.saad0@gmail.com>
2023-04-28 05:38:18 +0300

    m

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,48 @@
+# include "lists.h"
+# include <string.h>
+/**
+ * add_node - add node to list
+ * @head: linked list
+ * @str: string inserted
+ *
+ * Return: list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t *));
+	char *p;
+
+	if (str == NULL)
+		return (*head);
+	if (new)
+	{
+		p = strdup(str);
+		if (p)
+			new->str = strdup(str);
+		else
+			return (NULL);
+		new->len = _strlen(str);
+		if (*head == NULL)
+			new->next = NULL;
+		else
+			new->next = *head;
+		*head = new;
+	}
+	return (new);
+}
+
+/**
+ * _strlen - string length
+ * @s: string
+ * Return: size
+ */
+unsigned int _strlen(const char *s)
+{
+	unsigned int size = 0;
+
+	while (s[size])
+	{
+		size++;
+	}
+	return (size);
+}
