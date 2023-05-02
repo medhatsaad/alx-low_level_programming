eb1562803ed5b496f886a4414586b42be790cbb3
medhatsaad <medhat.saad0@gmail.com>
2023-05-03 02:56:29 +0300

    m

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,5 @@
 /**
- * print_listint - print all elements of list
+ * listint_len - print all elements of list
  * @h: list
  *
  * Return: number of elements
@@ -7,18 +7,16 @@
 # include "lists.h"
 # include <stdio.h>
 # include <stddef.h>
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	listint_t *p;
 	size_t i = 1;
 
 	if (h == NULL)
 		return (0);
-	printf("%d\n", h->n);
 	p = h->next;
 	while (p != NULL)
 	{
-		printf("%d\n", p->n);
 		p = p->next;
 		i++;
 	}
