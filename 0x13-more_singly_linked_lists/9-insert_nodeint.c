64127100189a291d0582d1009877f98be33c2688
medhatsaad <medhat.saad0@gmail.com>
2023-05-03 19:30:26 +0300

    m

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		y->n = n;
 		if (idx == 0)
 		{
-			y->next = (*head)->next;
+			y->next = *head;
 			*head = y;
 			return (y);
 		}
