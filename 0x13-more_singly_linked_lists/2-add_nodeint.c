a5892d5cac26eab8b8b8c9574edd233b7991b0e6
medhatsaad <medhat.saad0@gmail.com>
2023-05-03 03:26:38 +0300

    m

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,13 +1,13 @@
-/*
-  * add_nodeint - add node at beginning
-  * @head: list
-  * @n: value
-  *
-  * Return: list pointer
-  */
 # include "lists.h"
 # include <stdlib.h>
 # include <stdio.h>
+/**
+ * add_nodeint - add node at beginning
+ * @head: list
+ * @n: value
+ *
+ * Return: list pointer
+ */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *p;
