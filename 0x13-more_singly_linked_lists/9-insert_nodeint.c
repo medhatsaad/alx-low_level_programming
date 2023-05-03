24d355045cd342184861b30fde1852cecbb2ebb0
medhatsaad <medhat.saad0@gmail.com>
2023-05-03 05:53:11 +0300

    m

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,38 @@
+# include "lists.h"
+# include <stdlib.h>
+/**
+ * inser_nodeint_at_index - insert vlue n at index 
+ * @head: address of list
+ * @idx: index
+ * @n: value
+ *
+ * Return: address of new node or NULL
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *p = *head;
+	listint_t *y;
+	unsigned int i;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	for (i = 0; i < idx - 1; i++)
+	{
+		if (p->next == NULL)
+			break;
+		p = p->next;
+	}
+
+	if (i != idx - 1)
+		return (NULL);
+	y = malloc(sizeof(listint_t));
+	if (y)
+	{
+		y->n = n;
+		y->next = p->next;
+		p->next = y;
+		return (y);
+	}
+	else
+		return (NULL);
+}
