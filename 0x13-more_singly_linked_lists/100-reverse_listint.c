d759c9433cdd07669b71e7965a118a5d8acd84c5
medhatsaad <medhat.saad0@gmail.com>
2023-05-03 21:38:58 +0300

    m

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,26 +6,24 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
+	listint_t *n;
 	listint_t *temp;
-	listint_t *next_node;
-	listint_t *p;
 
 	if (head == NULL && *head == NULL)
 		return (NULL);
 	/* setup Node 0  */
-	p = *head;
-	next_node = p->next;
+	n = (*head)->next;
 	(*head)->next = NULL;
 	/* Setup nodes*/
-	while (next_node->next != NULL)
+	while (n->next != NULL)
 	{
-		temp = next_node->next;
-		next_node->next = p;
-		p = next_node;
-		next_node = temp;
+		temp = n->next;
+		n->next = *head;
+		*head = n;
+		n = temp;
 	}
 	/* setup last node*/
-	next_node->next = p;
-	*head = next_node;
+	n->next = *head;
+	*head = n;
 	return (*head);
 }
