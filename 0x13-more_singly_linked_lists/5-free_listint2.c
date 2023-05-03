7a4767a84b3d6d17bebf601998f5ab56d0bab7a5
medhatsaad <medhat.saad0@gmail.com>
2023-05-03 04:07:05 +0300

    m

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+# include "lists.h"
+# include <stdlib.h>
+/**
+ * free_listint2 - delete list
+ * @head: list address
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *p;
+
+	if (head != NULL)
+	{
+		if (*head != NULL)
+		{
+			while ((*head)->next != NULL)
+			{
+				p = (*head)->next;
+				free(*head);
+				*head = p;
+			}
+		}
+		free(*head);
+		head = NULL;
+	}
+}
