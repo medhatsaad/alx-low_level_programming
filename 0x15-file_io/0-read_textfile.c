5292b719f54463387467d86c42c077c1e38fa3ec
medhatsaad <medhat.saad0@gmail.com>
2023-05-10 21:09:20 +0300

    m

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,13 +11,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int a;
 	int fd = open(filename, O_RDONLY);
 
+	if (filename == NULL)
+		exit(0);
 	if (fd < 0)
 		exit(0);
 	buffer = malloc(letters);
 	if (!buffer)
 		exit(0);
 	a = read(fd, buffer, letters);
-	write(1, buffer, a);
+	if (a < 0)
+		exit(0);
+	a = write(1, buffer, a);
+	if (a < 0)
+		exit(0);
 	close(fd);
 	free(buffer);
 	return (a);
