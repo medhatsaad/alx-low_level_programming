a8cd753bfadc72cd6e51b5e012496e35ea80210f
medhatsaad <medhat.saad0@gmail.com>
2023-05-11 04:51:53 +0300

    m

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	for (i = 0; text_content[i] != '\0'; i++)
 		size++;
 	if (text_content == NULL)
-		return (1);
+		size = 0;
 	if (write(fd, text_content, size))
 		return (1);
 	else
