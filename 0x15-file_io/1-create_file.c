c83bdf33445fc7a8b8342ad6d8b6517e5ec11870
medhatsaad <medhat.saad0@gmail.com>
2023-05-11 04:39:04 +0300

    m

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,9 +12,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY);
+	fd = open(filename, O_WRONLY | O_TRUNC);
 	if (fd < 0)
-		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
+		fd = open(filename, O_WRONLY | O_CREAT, S_IRWXU);
 	if (fd < 0)
 		return (-1);
 	for (i = 0; text_content[i] != '\0'; i++)
